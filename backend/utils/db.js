03d23bbae2cf2c50be63912cadcc83850275787c
Ratan-444 <131332225+Ratan-444@users.noreply.github.com>
2025-09-22 14:45:59 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,13 +3,10 @@ import mongoose from "mongoose";
 
 let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
 
 async function connectDB() {
   if (cached.conn) return cached.conn;
-
   if (!cached.promise) {
     cached.promise = mongoose.connect(process.env.MONGO_URI).then((mongoose) => mongoose);
   }
